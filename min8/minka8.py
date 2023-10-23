65fca862c65a48c1f5bae5267672f5ea97813bfd
IvannZ <zhukov.ivan05@yandex.ru>
2023-10-23 09:11:02 +0700

    Add minka8

---
+++ min8/minka8.py
@@ -0,0 +1,40 @@
+import functools
+
+
+def deprecated(since=None, will_be_removed=None):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            name = func.__name__
+
+            if since is None and will_be_removed is None:
+                print(f"Warning: function {name} is deprecated. It will be removed in future versions.")
+            elif since is None:
+                print(f"Warning: function {name} is deprecated. It will be removed in version {will_be_removed}.")
+            elif will_be_removed is None:
+                print(
+                    f"Warning: function {name} is deprecated since version {since}. It will be removed in future versions.")
+            else:
+                print(
+                    f"Warning: function {name} is deprecated since version {since}. It will be removed in version {will_be_removed}.")
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+
+@deprecated(since="1.0", will_be_removed="5.0")
+def bar():
+    print("Hello from bar")
+
+
+@deprecated(will_be_removed="3.0")
+def baz():
+    print("Hello from baz")
+
+
+bar()
+baz()
