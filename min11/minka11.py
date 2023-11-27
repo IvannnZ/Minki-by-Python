a5d43a477ce2cbaf32d994e3fda9a36e1aa1de0d
IvannZ <zhukov.ivan05@yandex.ru>
2023-11-27 13:52:43 +0700

    add minka 11

--- min11/minka11.py
+++ min11/minka11.py
@@ -1,39 +1,40 @@
 class Singleton:
-    _instances = {}
-    def __init__(self):
+    _aaaa = False
+    _instances = None
 
     def __new__(cls, *args, **kwargs):
         print(cls, *args, **kwargs)
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__new__(cls)
+        if cls._instances == None:
+            cls._instances = super().__new__(cls)
+            cls.__init__(cls._instances, *args, **kwargs)
+            cls.__init__ = cls.not_init_
 
-        return cls._instances[cls]
+        return cls._instances
+
+    def not_init_(self, *args, **kwargs):
+        pass
 
 
 class Counter:
     """Special class to count things"""
 
-
     def __init__(self, initial_count=0, step=1):
         self.count = initial_count
         self.step = step
 
-
     def increment(self):
         self.count += self.step
+
 
 class Counter2:
     """Special class to count things"""
 
-
     def __init__(self, initial_count=0, step=1):
         self.count = initial_count
         self.step = step
 
-
     def increment(self):
         self.count += self.step
-
 
 
 class GlobalCounter(Singleton, Counter):
@@ -42,6 +43,7 @@ class GlobalCounter(Singleton, Counter):
 
 class GlobalCounter2(Singleton, Counter):
     pass
+
 
 gc1 = GlobalCounter()
 print(gc1.count)
@@ -53,11 +55,76 @@ print(gc2.count)
 print(gc1.count)
 print(id(gc1) == id(gc2))
 
+gc12 = GlobalCounter2()
+gc22 = GlobalCounter2()
+gc23 = GlobalCounter2()
 
+print(id(gc12) == id(gc22), id(gc23) == id(gc22))
+'''
+class Singleton:
+    _aaaa = False
+    _instances = None
+
+    def __new__(cls, *args, **kwargs):
+        print(cls, *args, **kwargs)
+        if cls._instances == None:
+            cls._instances = super().__new__(cls)
+            cls.__init__(cls._instances, *args, **kwargs)
+            cls.__init__ = cls.not_init_
+
+        return cls._instances
+
+    def not_init_(self, *args, **kwargs):
+        pass
+
+
+class Counter:
+    """Special class to count things"""
+
+    def __init__(self, initial_count=0, step=1):
+        self.count = initial_count
+        self.step = step
+
+    def increment(self):
+        self.count += self.step
+
+
+class Counter2:
+    """Special class to count things"""
+
+    def __init__(self, initial_count=0, step=1):
+        self.count = initial_count
+        self.step = step
+
+    def increment(self):
+        self.count += self.step
+
+
+class GlobalCounter(Singleton, Counter):
+    pass
+
+
+class GlobalCounter2(Singleton, Counter):
+    pass
+
+
+gc1 = GlobalCounter()
+print(gc1.count)
+gc1.increment()
+print(gc1.count)
+
+gc2 = GlobalCounter()
+print(gc2.count)
+print(gc1.count)
+print(id(gc1) == id(gc2))
 
 gc12 = GlobalCounter2()
 gc22 = GlobalCounter2()
 gc23 = GlobalCounter2()
 
-
 print(id(gc12) == id(gc22), id(gc23) == id(gc22))
+
+
+
+
+'''
