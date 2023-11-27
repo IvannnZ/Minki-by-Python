829f370479d7532506391afaa61373a66d876bde
IvannZ <zhukov.ivan05@yandex.ru>
2023-11-27 14:04:15 +0700

    add more logickal print in minka 8

--- min8/minka8.py
+++ min8/minka8.py
@@ -1,25 +1,27 @@
 import functools
+
 '''
 TODO
 убрать копипасту
 '''
+
 
 def deprecated(since=None, will_be_removed=None):
     def decorator(func):
         @functools.wraps(func)
         def wrapper(*args, **kwargs):
             name = func.__name__
-
-            if since is None and will_be_removed is None:
-                print(f"Warning: function {name} is deprecated. It will be removed in future versions.")
-            elif since is None:
-                print(f"Warning: function {name} is deprecated. It will be removed in version {will_be_removed}.")
-            elif will_be_removed is None:
-                print(
-                    f"Warning: function {name} is deprecated since version {since}. It will be removed in future versions.")
+            answer = f"Warning: function {name} is deprecated"
+            if since is None:
+                answer += ". It will be removed in"
             else:
-                print(
-                    f"Warning: function {name} is deprecated since version {since}. It will be removed in version {will_be_removed}.")
+                answer += f" since version {since}. It will be removed in"
+
+            if will_be_removed is None:
+                answer += " future versions."
+            else:
+                answer+=f" version {will_be_removed}."
+            print(answer)
 
             return func(*args, **kwargs)
 
