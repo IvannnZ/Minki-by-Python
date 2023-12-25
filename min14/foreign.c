1c309f76952566c1b5a94ab82bbb270148ea51bd
IvannZ <zhukov.ivan05@yandex.ru>
2023-12-25 15:48:44 +0700

    make all Mini task

--- min14/foreign.c
+++ min14/foreign.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <Python.h>
 
+int is_null(void *ptr){
+    return ptr == NULL;
+}
+
 void printMatrix(int **matrix, int size) {
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
@@ -28,8 +32,14 @@ void copyMatrix(int **src, int **dest, int size) {
 
 int **matrixMultiply(int **mat1, int **mat2, int size) {
     int **result = (int **)malloc(size * sizeof(int *));
+    if (is_null(**result)){
+        return NULL;
+    }
     for (int i = 0; i < size; i++) {
         result[i] = (int *)malloc(size * sizeof(int));
+        if(is_null(result[i])){
+            return NULL;
+        }
         for (int j = 0; j < size; j++) {
             result[i][j] = 0;
             for (int k = 0; k < size; k++) {
@@ -52,11 +62,17 @@ int **matrixDeg(int **matrix, int size, int power) {
         return result;
     } else if (power % 2 == 0) {
         int **temp = matrixDeg(matrix, size, power / 2);
+        if (is_null(temp)){
+            return NULL;
+        }
         int **result = matrixMultiply(temp, temp, size);
         freeMatrix(temp, size);
         return result;
     } else {
         int **temp = matrixDeg(matrix, size, power - 1);
+        if (is_null(temp)){
+            return NULL;
+        }
         int **result = matrixMultiply(matrix, temp, size);
         freeMatrix(temp, size);
         return result;
@@ -64,34 +80,35 @@ int **matrixDeg(int **matrix, int size, int power) {
 }
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-int main() {
-    int size = 3;
-    int deg = 5445;
-
-    int **matrix = (int **)malloc(size * sizeof(int *));
-    for (int i = 0; i < size; i++) {
-        matrix[i] = (int *)malloc(size * sizeof(int));
-        for (int j = 0; j < size; j++) {
-            matrix[i][j] = i * size + j + 1;
-        }
-    }
-
-    printf("Исходная матрица:\n");
-    printMatrix(matrix, size);
-
-    int **result = matrixDeg(matrix, size, deg);
-
-    printf("\nМатрица после возведения в степень %d:\n", deg);
-    printMatrix(result, size);
-
-    freeMatrix(matrix, size);
-    freeMatrix(result, size);
-
-    return 0;
-}
+//int main() {
+//    int size = 3;
+//    int deg = 5445;
+//
+//    int **matrix = (int **)malloc(size * sizeof(int *));
+//    for (int i = 0; i < size; i++) {
+//        matrix[i] = (int *)malloc(size * sizeof(int));
+//        for (int j = 0; j < size; j++) {
+//            matrix[i][j] = i * size + j + 1;
+//        }
+//    }
+//
+//    printf("Исходная матрица:\n");
+//    printMatrix(matrix, size);
+//
+//    int **result = matrixDeg(matrix, size, deg);
+//
+//    printf("\nМатрица после возведения в степень %d:\n", deg);
+//    printMatrix(result, size);
+//
+//    freeMatrix(matrix, size);
+//    freeMatrix(result, size);
+//
+//    return 0;
+//}
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
 static PyObject* foreign_matrix_power(PyObject* self, PyObject* args) {
+    //return Py_None;
     PyObject *py_matrix;
     PyObject *py_deg;
     if (!PyArg_ParseTuple(args, "OO", &py_matrix, &py_deg)) {
@@ -102,8 +119,14 @@ static PyObject* foreign_matrix_power(PyObject* self, PyObject* args) {
     int matrix_size = (int)PyObject_Length(py_matrix);
 
     int **matrix = (int **)malloc(matrix_size * sizeof(int *));
+    if(is_null(matrix)){
+        return Py_None;
+    }
     for (int i = 0; i < matrix_size; i++) {
         matrix[i] = (int *)malloc(matrix_size * sizeof(int));
+    if(is_null(matrix[i])){
+        return Py_None;
+    }
         for (int j = 0; j < matrix_size; j++) {
             matrix[i][j] = (int)PyLong_AsLong(PyList_GetItem(PyList_GetItem(py_matrix, i), j));
         }
@@ -114,6 +137,9 @@ static PyObject* foreign_matrix_power(PyObject* self, PyObject* args) {
 
     printf("deg:%d\n", degree);
     int **result = matrixDeg(matrix, matrix_size, degree);
+    if(is_null(result)){
+        return Py_None;
+    }
 
     PyObject *result_py = PyList_New(matrix_size);
     for (int i = 0; i < matrix_size; i++) {
@@ -124,8 +150,8 @@ static PyObject* foreign_matrix_power(PyObject* self, PyObject* args) {
         PyList_SetItem(result_py, i, sublist);
         free(matrix[i]);
     }
-    freeMatrix(matrix, size);
-    freeMatrix(result, size);
+    freeMatrix(matrix, matrix_size);
+    freeMatrix(result, matrix_size);
 
 
     return result_py;
